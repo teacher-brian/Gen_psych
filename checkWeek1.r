216e143acafa22176b195b208393179a49105b2d
teacher-brian <brian.holt@seattlecolleges.edu>
2022-10-13 10:58:53 -0700

    created another join to cross check with roster drops.  Next needs to include withdraw status

--- checkWeek1.r
+++ checkWeek1.r
@@ -1,3 +1,8 @@
+
+####note that the roster work needs to capture last column for 'withdraw'
+
+
+
 library(rvest)
 library(tidyverse)
 library(clipr)
@@ -8,7 +13,10 @@ users<- read.csv("slackUsers.csv")
 roster.d1 <- read.csv("d1.csv")
 roster.h2 <- read.csv("h2.csv")
 roster <- rbind(roster.d1,roster.h2)
-roster %>% separate(Name,into = c('last','first'),sep = ',')
+roster <- roster[complete.cases(roster),]
+rownames(roster) <- NULL
+
+roster<- roster %>% separate(Name,into = c('last','first'),sep = ',')
 
 email<- as.data.frame(strsplit(email,", "))
 email1<- as.data.frame(strsplit(email1,", "))
@@ -34,11 +42,6 @@ head(week_post)
 
 
 
-grep("[a]|\\s",week1,value = T)  %>%
-  grep("[a-z]$|\\s[0-9]{1,2}:.*[AP]M$|joined",.,value = T,ignore.case = T)  %>%
-  grep("Google Doc|G Suite Document|repl|days|iew|http|rian|Only|one doc",
-       .,invert = T,value = T)  %>%  as.data.frame() -> week_post
-colnames(week_post) <- "data"
 posted <- week_post %>%
   mutate(time=dplyr::lead(data,1),joined=dplyr::lead(data,2)) %>%
   mutate(time= gsub("^[a-zA-Z]",NA,time)) %>%
@@ -48,12 +51,12 @@ posted <- week_post %>%
   filter(complete.cases(.)) %>%
   select(data) %>%  unique(.)
 
+users  %>% mutate_all(as.character) %>%
+  mutate(displayname=ifelse(nchar(displayname)<1,fullname,displayname)) %>%
+  anti_join(posted, by = c("displayname"="data")) %>%
+  filter(status =="Member") %>% separate(fullname,into=c("first",'last')," ") %>%   left_join(roster)
 
 
-colnames(week_post) <- "data"
-
-
-head(week_post)
 
 users  %>% mutate_all(as.character) %>% mutate(displayname=ifelse(nchar(displayname)<1,fullname,displayname)) %>% anti_join(posted, by = c("displayname"="data")) %>% filter(status =="Member") %>% select(displayname,email,fullname) %>%
   mutate(message= paste0("Hi @",displayname,", Week 3 is beginning, and I'm not seeing a post from you to week 1. I might be wrong because I'm using a program to filter for people who haven't posted yet, but I don't think you've posted to week 1 yet.  Do you need any help?  I know a few students are trying to get caught up but at this point, week 1 should be posted.  The #logistics channel is available."))  %>% write_clip()
