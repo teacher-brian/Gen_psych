8b8ead60734b9bfa5722330c1d2b7f24028c6672
teacher-brian <brian.holt@seattlecolleges.edu>
2022-11-17 15:03:11 -0800

    updating self reflection check

--- Self-reflection_complete.R
+++ Self-reflection_complete.R
@@ -12,13 +12,13 @@ library(kableExtra)
 s<- read_sheet('https://docs.google.com/spreadsheets/d/1skEgkolZpr4r_iMaBM0qMxtuNAKATgyB0X2QDSpzor8/edit?resourcekey#gid=271104061')
 #rename columns
 
-variables <- c('Timestamp','l_name','f_name','class','aspects','effort','grade','concerns','thoughts?','reflect_num')
+variables <- c('Timestamp','l_name','f_name','class','aspects','effort','grade','concerns','thoughts?','reflect_num','workflow')
 
 colnames(s) <- variables
 
 #%>% s[!(duplicated(s$l_name) & duplicated(s$f_name)),]
 
-(p<- s %>% filter(Timestamp>"2022-04-01") %>%
+(p<- s %>% filter(Timestamp > "2022-10-01") %>%
     #select(f_name,l_name,Timestamp) %>%
     arrange(-desc(Timestamp)) %>% mutate(l_name=toupper(l_name))) %>% arrange(Timestamp) %>%  print(.,n=100)
 
@@ -45,34 +45,36 @@ v4 <- v4[-1]
 
 
 # maybe add a new colum with a repetatin number 1-11
-d <- d1 %>% unlist %>% as.data.frame() %>%
-  mutate(col_n = rep(1:10,length.out=length(.))) %>%  #if 11 columns, rep 1:11
-#group_by(col_n) %>%
+d <-
+  d1 %>% unlist %>% as.data.frame() %>%
+  mutate(col_n = rep(1:11,length.out=length(.))) %>%  #if 11 columns, rep 1:11
+
+group_by(col_n) %>%
     rename('title'=1,) %>%
-#mutate(row = row_number())%>%
+mutate(row = row_number())%>%
   pivot_wider(
               names_from=col_n,
               values_from = title
                )  %>% unchop(everything())
-d <- d %>% mutate(Status="",section="d1")  #note if there are 10 or 11 columns
-colnames(d) <- c("Notify","blank","Photo","ID","Name","Basis", "Units","Program","area","Level", "Status","section" )
+d <- d %>% mutate(section="d1") %>% #note if there are 10 or 11 columns
+  rename("row"=1,"Notify"=2,"blank"=3,"Photo"=4,"ID"=5,"Name"=6,"Basis"=7, "Units"=8,"Program"=9,"area"=10,"Level"=11, "Status"=12,"section"=13 )
 
-d %>% select(4,5,11,12)
+d %>% select(5,6,12,13)
 
 
 
 
 v <- v2 %>% unlist %>% as.data.frame() %>%
   mutate(col_n = rep(1:11,length.out=length(.))) %>%
-  #group_by(col_n) %>%
+  group_by(col_n) %>%
   rename('title'=1,) %>%
-  #mutate(row = row_number())%>%
+  mutate(row = row_number())%>%
   pivot_wider(
     names_from=col_n,
     values_from = title
   )  %>% unchop(everything())
-v <- v %>% mutate(section="v2")
-colnames(v) <- c("Notify","blank","Photo","ID","Name","Basis", "Units","Program","area","Level", "Status","section" )
+v <- v %>%mutate(section="v2") %>% #note if there are 10 or 11 columns
+  rename("row"=1,"Notify"=2,"blank"=3,"Photo"=4,"ID"=5,"Name"=6,"Basis"=7, "Units"=8,"Program"=9,"area"=10,"Level"=11, "Status"=12,"section"=13 )
 
 
 va <- v4 %>% unlist %>% as.data.frame() %>%
@@ -94,19 +96,21 @@ w <- "Withdrawn"
 
 ##  Currently withdrawn students  ###
 
-rbind(v %>% filter(Status==w) %>% select(4,5,11,12),va %>% filter(Status==w) %>% select(4,5,11,12),d %>% filter(Status==w) %>% select(4,5,11,12))
+rbind(v %>% filter(Status==w) %>% select(5,6,12,13),
+      #va %>% filter(Status==w) %>% select(4,5,11,12),
+      d %>% filter(Status==w) %>% select(5,6,12,13))
 
 ##  Currently enrolled students to be joined with reflection data###
 
 reflect <- rbind(d %>%
                    filter(Status!=w) %>%
-                   select(4,5,12,11),
+                   select(5,6,12,13),
                  v %>%
                    filter(Status!=w) %>%
-                   select(4,5,12,11),
-                 va %>%
-                   filter(Status!=w) %>%
-                   select(4,5,12,11)
+                   select(5,6,12,13)
+                 #va %>%
+                  # filter(Status!=w) %>%
+                   #select(4,5,12,11)
 )
 
   ####  Currently enrolled students who have NOT done reflection for week 3  ###
@@ -116,7 +120,8 @@ reflect %>%  # Roster data
   mutate(l_name=toupper(l_name)) %>%
   left_join(p,by='l_name')  %>%    # joins with self reflection survey
   filter(is.na(reflect_num)|reflect_num == "Week 9 Self reflection") %>%
-  mutate(message=paste0("\n\n","Hello ",f_name.x,",","\n\n","This is a hard message to send.",'\n\n'," You're receiving it to let you know that I haven't heard from you and that you haven't done the first self-reflection that was due Sunday, Jan 23rd 11:59pm. In an effort to keep you on track, I imposed this deadline so that issues could be handled before the end of the quarter.  As of right now, you're a day late and I'm willing to Grant an additional day but at this point it's looking like you will not be able to get better than a B for the quarter. It's still expected that you do the work and that you are not late for the next 3 week deadline. If you miss that deadline you won't be able to get better than a C.","\n\n","Let me know if you can get it done in the next day.","\n\n")) %>% print(.,n=100) %>% write_clip()
+  select(1:5,13) %>%
+  mutate(message=paste0("\n\n","Hello ",f_name.x,",","\n\n","This is a hard message to send.",'\n\n'," You're receiving it to let you know that you haven't done the first self-reflection that was due at the end of week 5, more or less. What makes this very hard is that tomorrow, November 18th, is the last day of the quarter to withdraw from the class, which is why I'm alerting you today. ","\n\n","A withdrawal leaves a 'w' on your transcript, but it doesn't affect your GPA.  After Friday, 11/18/22, you will not be able to withdraw, and so my worry is that you might not have enough time to catch up with quality work before the end of the quarter.","\n\n", "**I'm not saying you should withdraw.  Several students are probably working on week 5 and the self reflection this week or weekend.  But I am saying that your choice to continue or withdraw must be made by tomorrow.**  There are still 3-4 weeks in the quarter and for many students, they'll be able to finish up with good enough quality for a passing grade (which is anything above a 1.0).","\n\n","But I suspect that a few students might want to withdraw.  You can contact me with questions; I'll do my best to reply before tomorrow's deadline.\n\n")) %>% print(.,n=100) %>% write_clip()
 
 
   ####  Currently enrolled students who have NOT done reflection for week 6  ###
