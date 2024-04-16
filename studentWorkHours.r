7f491e7840777af23ff6911be65920293b3394cd
Holt <Brian.Holt@seattlecolleges.edu>
2024-04-16 10:35:59 -0700

    removed week factor from descriptions

--- studentWorkHours.r
+++ studentWorkHours.r
@@ -8,6 +8,6 @@ str(sh)
 colnames(sh) <- c("timestamp","student_class","week","psych_hrs","class2_hrs","class3_hrs","class4_hrs","class5_hrs","hs_hrs","work_hrs")
 sh[,2:3] <- lapply(sh[,2:3],as.factor)
 
-sh %>% group_by(student_class,week) %>%
+sh %>% group_by(student_class) %>%
   summarise(across(ends_with("hrs"),  list(mean = mean, sd = sd, max = max ), na.rm = TRUE)) %>% t()
 
