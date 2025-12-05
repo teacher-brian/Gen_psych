429c721541a2554d0641b045d194ef4fb0515b0e
Holt <Brian.Holt@seattlecolleges.edu>
2025-12-05 13:20:26 -0800

    initial commit

---
+++ Student_Survey_Tracker.R
@@ -0,0 +1,28 @@
+library(googlesheets4)
+library(tidyverse)
+
+
+ts_raw<- read_sheet("https://docs.google.com/spreadsheets/d/11zT13iBZw3QIIQDS1j8GJnI4GMmHo_S-oxfL1dGPssU/edit?usp=sharing",range= "Form Responses 1")
+ts <- ts_raw
+
+dfts<- data.frame((apply(ts[,5:53],2,function(x) sum(!is.na(x)))))
+
+colnames(dfts) <- "count"
+dfts %>%
+  rownames_to_column("form") %>%
+  mutate(form = fct_reorder(form, count)) %>%
+
+  # arrange(desc(count)) %>%
+  ggplot(aes(x=form,y=count))+
+
+  geom_col()+
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
+
+
+
+
+
+# #
+#
+# ts %>% summarise(across(`form 1`: `form 49`, ~ count(.x)))
+# # summarise(across(c(col1, col2, col3), ~sum(is.na(.)), .names = "na_count_{.col}"))
