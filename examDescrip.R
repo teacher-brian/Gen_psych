9e44e13355b5a6ffc078ac6ca1b417ea32e09481
Holt <Brian.Holt@seattlecolleges.edu>
2026-01-29 14:57:01 -0800

    added student exam

--- examDescrip.R
+++ examDescrip.R
@@ -16,10 +16,14 @@ colnames(e2) <- colnames(e1)
 colnames(e3) <- colnames(e1)
 
 
-exam1Win26<- rbind(e1,e2,e3)
-# write.csv(exam1Win26,"exam1Win26.csv",row.names = F)
+exam1Win26<- rbind(e1,e3)
 
-# exam1Win26 <- read.csv("exam1Win26.csv")
+exam1Win26 <- rbind(e1[e1$class!="1pm",],e3)
+
+
+ # write.csv(e1,"exam1Win26.csv",row.names = F)
+
+e1 <- read.csv("exam1Win26.csv")
 
 mean(exam1Win26$exam.1,na.rm=T)
 quantile(exam1Win26$exam.1)
@@ -32,7 +36,7 @@ e1 <- exam1Win26
 e1 %>% group_by(class) %>% mutate(across(exam.1:sa1,~scale(.))) %>%
   summarize(corr=cor(e1$exam.1,e1$sa1,use="pairwise.complete.obs"))
 
-cor(e1$exam.1[e1$class=="1pm"],e1$sa1[e1$class=="1pm"],use = "pairwise.complete.obs")
+# cor(e1$exam.1[e1$class=="1pm"],e1$sa1[e1$class=="1pm"],use = "pairwise.complete.obs")
 
 
 sims <- 1e4
@@ -40,3 +44,13 @@ sims <- 1e4
 e1mc<- sample(e1$exam1,sims,replace = T)
 hist(e1mc)
 mean(e1mc)
+
+
+
+
+
+e1 %>% group_by(class) %>%
+  summarise(mean=mean(exam.1,na.rm=T),
+            sd=sd(exam.1,na.rm=T),
+            med=median(exam.1,na.rm=T),
+            n=n())
