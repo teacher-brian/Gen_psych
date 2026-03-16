917978383cbe1505b87331d0c000a1c996df5e74
Holt <Brian.Holt@seattlecolleges.edu>
2026-03-16 14:32:08 -0700

    made survey questions ;

--- student_generated_questions.R
+++ student_generated_questions.R
@@ -7,3 +7,18 @@ colnames(q)
 q %>% group_by(`Please select your class`) %>%
   arrange( `Please select your class`,`What is your name as in canvas`) %>%
   print(n=100)
+
+
+colnames(q) <- c("time","name","class","q1","q1answers","q2","q2answers","q3","q3answers")
+
+q %>% pivot_longer(cols=c(4,6,8),names_to = "q_num",values_to = "q")
+
+
+q %>%
+  rename_with(~ sub("^q(\\d+)$", "question\\1", .x)) %>%
+  rename_with(~ sub("^q(\\d+)answers$", "answers\\1", .x)) %>%
+  pivot_longer(
+    cols = matches("^(question|answers)\\d+$"),
+    names_to = c(".value", "item"),
+    names_pattern = "^(question|answers)(\\d+)$"
+  )
